04722ace2c73057a8102acaf0f5dabd440500fae
Kristawan Jaroenrad <kristawanja@gmail.com>
2025-04-30 00:16:02 +0700

    Switch to replacing missing value with KNN imputer

--- training/prepare_data.py
+++ training/prepare_data.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 
 import pandas as pd
+from sklearn.impute import KNNImputer
+imputer = KNNImputer(n_neighbors=2)
 
 
 class PrepareData:
@@ -11,13 +13,23 @@ class PrepareData:
         .resample("6H")
         .mean()
     )
+    # Using KNN Imputer to replace the missing values, this should work better than replacing with previous value.
+    indexes = df.index
+    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+    df.index = indexes
     train_end = datetime(2025, 4, 9)
     test_end = datetime(2025, 4, 11)
 
     @classmethod
     def train_data(cls) -> pd.DataFrame:
-        return cls.df[: cls.train_end - timedelta(days=1)]
+        return cls.df[: cls.train_end - timedelta(hours=6)]
 
     @classmethod
     def test_data(cls) -> pd.DataFrame:
         return cls.df[cls.train_end : cls.test_end]
+    
+    
+if __name__ == "__main__":
+
+    test = PrepareData.test_data()
+    print(test)
